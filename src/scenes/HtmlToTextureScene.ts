111cea0a0540a27e6048e6f025802c03c13bafee
Roland Csibrei <roland.csibrei@nascortech.com>
2021-09-15 18:51:57 +0200

    Fixed offsets

--- src/scenes/HtmlToTextureScene.ts
+++ src/scenes/HtmlToTextureScene.ts
@@ -56,12 +56,54 @@ export class HtmlToTextureScene extends BaseScene {
 
   private _reflectionTexture?: MirrorTexture
   private _reflectionTextureRenderList: Nullable<AbstractMesh[]> = null
+  private _oldSelectedHtmlElement: any
 
-  private get _arcCamera() {
-    return <ArcRotateCamera>this._camera
+  public async showDebugLayer() {
+    await this._scene.debugLayer.show({
+      embedMode: false,
+      overlay: true
+    })
   }
 
-  private _oldSelectedHtmlElement: any
+  public async hideDebugLayer() {
+    await this._scene.debugLayer.hide()
+  }
+
+  public setTexture1(textureBuffer: ArrayBufferView | undefined) {
+    if (textureBuffer) {
+      if (!this._htmlTexture1) {
+        const texture = new RawTexture(textureBuffer, PANEL_WIDTH, PANEL_HEIGHT, Engine.TEXTUREFORMAT_RGBA, this._scene, false, true)
+        this._htmlTexture1 = texture
+      } else {
+        this._htmlTexture1.update(textureBuffer)
+      }
+
+      if (this._htmlMaterial1) {
+        this._htmlMaterial1.emissiveTexture = this._htmlTexture1
+      }
+    } else {
+      console.warn('TextureBuffer1 disconnected')
+    }
+  }
+
+  public setTexture2(textureBuffer: ArrayBufferView | undefined) {
+    if (textureBuffer) {
+      if (!this._htmlTexture2) {
+        const texture = new RawTexture(textureBuffer, PANEL_WIDTH, PANEL_HEIGHT, Engine.TEXTUREFORMAT_RGBA, this._scene, false, true)
+        this._htmlTexture2 = texture
+      } else {
+        this._htmlTexture2.update(textureBuffer)
+      }
+
+      if (this._htmlMaterial2) {
+        this._htmlMaterial2.emissiveTexture = this._htmlTexture2
+      }
+    } else {
+      console.warn('TextureBuffer2 disconnected')
+    }
+  }
+
+  //
 
   constructor(canvas: HTMLCanvasElement) {
     super(canvas)
@@ -165,17 +207,14 @@ export class HtmlToTextureScene extends BaseScene {
     })
 
     this._scene.onPointerPick = (e, pickInfo) => {
-      const element = this._getHtmlElementFromPickInfo(pickInfo, true)
-      if (element) {
-        setTimeout(() => {
-          // element.dispatchEvent(new MouseEvent('click', { shiftKey: false }))
-          // console.log('Click at', e.currentTarget, element, htmlCoordsX, htmlCoordsY)
-          // element.parentElement?.click()
-          // element.click()
-          // element.getRootNode().click()
-        }, 100)
-      }
+      this._getHtmlElementFromPickInfo(pickInfo, true) // simulate click
     }
+  }
+
+  //
+
+  private get _arcCamera() {
+    return <ArcRotateCamera>this._camera
   }
 
   private _positionFakeCursor(htmlX: number, htmlY: number) {
@@ -189,20 +228,14 @@ export class HtmlToTextureScene extends BaseScene {
   private _getHtmlElementFromPickInfo(pickInfo: PickingInfo, simulateClick = false) {
     const cords = pickInfo.getTextureCoordinates()
     if (cords) {
-      const offsetX = pickInfo.pickedMesh?.name === 'htmlPlane2' ? -PANEL_WIDTH - 150 : -1920
-      const offsetY = 70
+      const offsetX = pickInfo.pickedMesh?.name === 'htmlPlane2' ? WIDTH - PANEL_WIDTH : 0
 
-      const htmlX = 1920 + offsetX + cords.x * PANEL_WIDTH
+      const htmlX = offsetX + cords.x * PANEL_WIDTH
       const htmlY = PANEL_HEIGHT - cords.y * PANEL_HEIGHT - 70
       const elements = document.elementsFromPoint(htmlX, htmlY)
       const element = elements[1]
       if (element instanceof HTMLElement) {
         if (simulateClick) {
-          // const tree = document.getElementById('tree')
-          // debugger
-          // if (tree) {
-          //   tree.click()
-          // }
           this._simulateClick(element, htmlX, htmlY)
         }
 
@@ -217,186 +250,16 @@ export class HtmlToTextureScene extends BaseScene {
   }
 
   private _simulateClick(element: HTMLElement, x: number, y: number) {
-    // element.click()
-
     var evt = document.createEvent('MouseEvents')
     x = 0
     y = 0
     evt.initMouseEvent('click', true, true, window, 0, x, y, x, y, false, false, false, false, 0, null)
     element.dispatchEvent(evt)
+
     console3d.log('Simulated click on', `${element.tagName}#${element.id}.${element.className}`)
   }
 
-  createCamera() {
-    const camera = new ArcRotateCamera('camera', -1.24, 1.43, 30, new Vector3(0, 0, 0), this._scene)
-    camera.attachControl(this._canvas, true)
-    camera.minZ = 0.05
-    camera.maxZ = 1000
-    this._camera = camera
-  }
-
-  createLight(scene: Scene) {
-    const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene)
-    light.intensity = 0.7
-  }
-
-  private async _createSkyBox(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      const skybox = MeshBuilder.CreateBox('skyBox', { size: 100.0 }, this._scene)
-      const skyboxMaterial = new StandardMaterial('skyBox', this._scene)
-      skyboxMaterial.backFaceCulling = false
-      const files = [
-        'textures/space_left.jpg',
-        'textures/space_up.jpg',
-        'textures/space_front.jpg',
-        'textures/space_right.jpg',
-        'textures/space_down.jpg',
-        'textures/space_back.jpg'
-      ]
-      const reflectionTexture = CubeTexture.CreateFromImages(files, this._scene)
-      // not working
-      // const reflectionTexture = new CubeTexture('', this._scene, null, undefined, files, () => {
-
-      reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE
-      skyboxMaterial.reflectionTexture = reflectionTexture
-      skyboxMaterial.disableLighting = true
-      skyboxMaterial.diffuseColor = new Color3(0, 0, 0)
-      skyboxMaterial.specularColor = new Color3(0, 0, 0)
-      skybox.material = skyboxMaterial
-      resolve()
-      // })
-
-      setTimeout(() => {
-        reject()
-      }, 60000)
-    })
-  }
-
-  public async initScene() {
-    this._scene.clearColor = new Color4(0, 0, 0, 0)
-    this.createCamera()
-    this.createLight(this._scene)
-
-    await this._createDemo()
-    await this.screenshot()
-  }
-
-  private async _createDemo() {
-    await this._createSkyBox()
-    this._createObjects()
-  }
-
-  private _createObjects() {
-    const parent = new TransformNode('objects-parent', this._scene)
-    parent.position.z = 10
-
-    const boxMaterial = new StandardMaterial('boxMaterial', this._scene)
-    boxMaterial.emissiveColor = Color3.Red().scale(0.4)
-    boxMaterial.disableLighting = true
-
-    const box = Mesh.CreateBox('box', 4, this._scene)
-    box.parent = parent
-    box.material = boxMaterial
-    box.position.x = -8
-
-    //
-
-    const sphereMaterial = new StandardMaterial('sphereMaterial', this._scene)
-    sphereMaterial.emissiveColor = Color3.Green().scale(0.4)
-    sphereMaterial.disableLighting = true
-
-    const sphere = Mesh.CreateSphere('box', 32, 4, this._scene)
-    sphere.parent = parent
-    sphere.material = sphereMaterial
-
-    //
-
-    const kokkiMaterial = new StandardMaterial('kokkiMaterial', this._scene)
-    kokkiMaterial.emissiveColor = Color3.Blue().scale(0.4)
-    kokkiMaterial.disableLighting = true
-
-    const kokki = MeshBuilder.CreateCylinder('kokki', { diameterTop: 0, diameterBottom: 4, height: 4 }, this._scene)
-    kokki.parent = parent
-    kokki.material = kokkiMaterial
-    kokki.position.x = 8
-  }
-
-  public async showDebugLayer() {
-    await this._scene.debugLayer.show({
-      embedMode: false,
-      overlay: true
-    })
-  }
-
-  public async hideDebugLayer() {
-    await this._scene.debugLayer.hide()
-  }
-
-  public setTexture0(textureBuffer: Blob | ArrayBuffer | undefined) {
-    if (textureBuffer) {
-      const old = this._htmlTexture1
-      old?.dispose()
-
-      const texture = new Texture(
-        'data:htmlTexture1',
-        this._scene,
-        true,
-        true,
-        Engine.TEXTURE_LINEAR_LINEAR,
-        null,
-        () => console.log('Error'),
-        textureBuffer,
-        false,
-        Engine.TEXTUREFORMAT_RGBA
-      )
-
-      // this._htmlTexture1 = texture
-
-      if (this._htmlMaterial1) {
-        this._htmlMaterial1.emissiveTexture = texture
-      }
-    } else {
-      console.log('TextureBuffer1 disconnected')
-    }
-  }
-
-  public setTexture1(textureBuffer: ArrayBufferView | undefined) {
-    if (textureBuffer) {
-      if (!this._htmlTexture1) {
-        const texture = new RawTexture(textureBuffer, PANEL_WIDTH, PANEL_HEIGHT, Engine.TEXTUREFORMAT_RGBA, this._scene, false, true)
-        this._htmlTexture1 = texture
-      } else {
-        this._htmlTexture1.update(textureBuffer)
-      }
-
-      if (this._htmlMaterial1) {
-        this._htmlMaterial1.emissiveTexture = this._htmlTexture1
-      }
-    } else {
-      console.log('TextureBuffer1 disconnected')
-    }
-  }
-
-  public setTexture2(textureBuffer: ArrayBufferView | undefined) {
-    if (textureBuffer) {
-      if (!this._htmlTexture2) {
-        const texture = new RawTexture(textureBuffer, PANEL_WIDTH, PANEL_HEIGHT, Engine.TEXTUREFORMAT_RGBA, this._scene, false, true)
-        this._htmlTexture2 = texture
-      } else {
-        this._htmlTexture2.update(textureBuffer)
-      }
-
-      if (this._htmlMaterial2) {
-        this._htmlMaterial2.emissiveTexture = this._htmlTexture2
-      }
-    } else {
-      console.log('TextureBuffer2 disconnected')
-    }
-  }
-
-  async screenshot() {
-    await this.showDebugLayer()
-
+  private async _startCapture() {
     const video = document.createElement('video')
     let captureStream: MediaStream | null = null
     const stopCapture = () => {
@@ -481,56 +344,100 @@ export class HtmlToTextureScene extends BaseScene {
     })
   }
 
-  //   function simulate(element, eventName, options) {
-  //     var opt = {
-  //         pointerX: 0,
-  //         pointerY: 0,
-  //         button: 0,
-  //         ctrlKey: false,
-  //         altKey: false,
-  //         shiftKey: false,
-  //         metaKey: false,
-  //         bubbles: true,
-  //         cancelable: true
-  //     }
-  //     Object.assign(opt, options)
+  createCamera() {
+    const camera = new ArcRotateCamera('camera', -1.24, 1.43, 30, new Vector3(0, 0, 0), this._scene)
+    camera.attachControl(this._canvas, true)
+    camera.minZ = 0.05
+    camera.maxZ = 1000
+    this._camera = camera
+  }
 
-  //     var oEvent
-  //     switch (eventName) {
-  //         case "unload":
-  //         case "abort":
-  //         case "error":
-  //         case "select":
-  //         case "change":
-  //         case "submit":
-  //         case "reset":
-  //         case "focus":
-  //         case "blur":
-  //         case "resize":
-  //         case "scroll":
-  //             oEvent = document.createEvent("HTMLEvents")
-  //             oEvent.initEvent(eventName, opt.bubbles, opt.cancelable)
-  //             break
+  createLight(scene: Scene) {
+    const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene)
+    light.intensity = 0.7
+  }
 
-  //         case "click":
-  //         case "dblclick":
-  //         case "mousedown":
-  //         case "mouseup":
-  //         case "mouseover":
-  //         case "mousemove":
-  //         case "mouseout":
-  //         case "keydown":
-  //         case "keyup":
-  //             oEvent = document.createEvent("MouseEvents")
-  //             oEvent.initMouseEvent(eventName, opt.bubbles, opt.cancelable, document.defaultView,
-  //                 opt.button, opt.pointerX, opt.pointerY, opt.pointerX, opt.pointerY,
-  //                 opt.ctrlKey, opt.altKey, opt.shiftKey, opt.metaKey, opt.button, element)
-  //             break
+  private async _createSkyBox(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const skybox = MeshBuilder.CreateBox('skyBox', { size: 100.0 }, this._scene)
+      const skyboxMaterial = new StandardMaterial('skyBox', this._scene)
+      skyboxMaterial.backFaceCulling = false
+      const files = [
+        'textures/space_left.jpg',
+        'textures/space_up.jpg',
+        'textures/space_front.jpg',
+        'textures/space_right.jpg',
+        'textures/space_down.jpg',
+        'textures/space_back.jpg'
+      ]
+      const reflectionTexture = CubeTexture.CreateFromImages(files, this._scene)
+      // not working
+      // const reflectionTexture = new CubeTexture('', this._scene, null, undefined, files, () => {
 
-  //         default:
-  //             throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported')
-  //     }
+      reflectionTexture.coordinatesMode = Texture.SKYBOX_MODE
+      skyboxMaterial.reflectionTexture = reflectionTexture
+      skyboxMaterial.disableLighting = true
+      skyboxMaterial.diffuseColor = new Color3(0, 0, 0)
+      skyboxMaterial.specularColor = new Color3(0, 0, 0)
+      skybox.material = skyboxMaterial
+      resolve()
+      // })
 
-  //     element.dispatchEvent(oEvent)
-  // }
+      setTimeout(() => {
+        reject()
+      }, 60000)
+    })
+  }
+
+  public async initScene() {
+    this._scene.clearColor = new Color4(0, 0, 0, 0)
+    this.createCamera()
+    this.createLight(this._scene)
+
+    await this._createDemo()
+
+    await this.showDebugLayer()
+
+    await this._startCapture()
+  }
+
+  private async _createDemo() {
+    await this._createSkyBox()
+    this._createObjects()
+  }
+
+  private _createObjects() {
+    const parent = new TransformNode('objects-parent', this._scene)
+    parent.position.z = 10
+
+    const boxMaterial = new StandardMaterial('boxMaterial', this._scene)
+    boxMaterial.emissiveColor = Color3.Red().scale(0.4)
+    boxMaterial.disableLighting = true
+
+    const box = Mesh.CreateBox('box', 4, this._scene)
+    box.parent = parent
+    box.material = boxMaterial
+    box.position.x = -8
+
+    //
+
+    const sphereMaterial = new StandardMaterial('sphereMaterial', this._scene)
+    sphereMaterial.emissiveColor = Color3.Green().scale(0.4)
+    sphereMaterial.disableLighting = true
+
+    const sphere = Mesh.CreateSphere('box', 32, 4, this._scene)
+    sphere.parent = parent
+    sphere.material = sphereMaterial
+
+    //
+
+    const kokkiMaterial = new StandardMaterial('kokkiMaterial', this._scene)
+    kokkiMaterial.emissiveColor = Color3.Blue().scale(0.4)
+    kokkiMaterial.disableLighting = true
+
+    const kokki = MeshBuilder.CreateCylinder('kokki', { diameterTop: 0, diameterBottom: 4, height: 4 }, this._scene)
+    kokki.parent = parent
+    kokki.material = kokkiMaterial
+    kokki.position.x = 8
+  }
 }
